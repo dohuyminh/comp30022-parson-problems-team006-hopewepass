8e8ac00e40cebf2ba0fab002226d7f0d93c87dfb
ntddat <ntdat401161@gmail.com>
2024-08-21 11:34:33 +1000

    changed the prompt and formatted the code gemini sends back a little bit

--- gemini.js
+++ gemini.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config()
 const { GoogleGenerativeAI } = require('@google/generative-ai');
+const { PythonShell } = require('python-shell')
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
@@ -12,16 +13,40 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 // Correlation, Linear Regression, Decision Tree Classifier, Read/Write CSV files
 
 async function run() {
-  var prompt = "Create a Python script about reading and writing CSV files with the context being koalas without any code comments";
+  var prompt = "Create a Python script with the following requirements:\n";
+  // prompt = prompt.concat("- The code should be 10 lines long\n- It should have a description of the code after the snippet\n-It should be about linear regression\n- It should be about the animal koala");
+  prompt = prompt.concat("- The code should be 10 lines long\n-It should be about linear regression\n- It should be about the animal koala\n- It should not have any comments in the code");
   // Starting a full chat
   const chat = model.startChat({ history: [] })
   let result = await chat.sendMessage(prompt);
-  console.log(result.response.text());
+  var response = result.response.text();
+  console.log(response);
+  response = formatCode(response);
+  console.log(response);
+  PythonShell.runString(response, null).then(messages=>{
+    console.log("Output:\n");
+    console.log(messages);
+  });
 
-  prompt = prompt.concat("that's different from the last one");
+  prompt = prompt.concat("\n- It should be different from any previous scripts");
 
   result = await chat.sendMessage(prompt);
   console.log(result.response.text());
+}
+
+function formatCode(response) {
+  response = response.replaceAll("`", "");
+  for (let i = 0; i < response.length; i++) {
+    response = response.slice(0, 0) + response.slice(1)
+   if (response.charAt(0) == '\n') {
+      break;
+    }
+  }
+  return response;
+}
+
+function separateCodeAndDesc(response) {
+  
 }
 
 run();
